76d70bc32e6f178fa31078172232df627a7e6fd3
Vinicius Rodrigues Cordeiro <vrc_crv@outlook.com.br>
2026-04-28 07:08:56 -0300

    gerar metodo que chame a service, carregar tarefa e excluir tarefa

--- src/app/components/tarefa-lista/tarefa-lista.component.ts
+++ src/app/components/tarefa-lista/tarefa-lista.component.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
+import { TarefaService } from '../../services/tarefa.service';
+import { Tarefa } from '../../models/tarefa.model';
 
 @Component({
   selector: 'app-tarefa-lista',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './tarefa-lista.component.html',
   styleUrl: './tarefa-lista.component.css'
 })
-export class TarefaListaComponent {
+export class TarefaListaComponent implements OnInit {
+
+  tarefas: Tarefa[] = [];
+
+  constructor(private tarefaService: TarefaService) { }
+
+  ngOnInit(): void {
+    this.carregarTarefas();
+  }
+
+  carregarTarefas(): void {
+    this.tarefaService.listarTarefas().subscribe(dados => {
+      this.tarefas = dados;
+    });
+  }
+
+  excluir(id: number): void {
+    if (confirm('Deseja excluir esta tarefa?')) {
+      this.tarefaService.excluir(id).subscribe(() => {
+        this.carregarTarefas();
+      });
+    }
+  }
 
 }
