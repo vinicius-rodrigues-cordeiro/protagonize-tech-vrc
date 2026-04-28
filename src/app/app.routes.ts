41a51cdc637e6e5fc50bd7c86b223482c4b69e7e
Vinicius Rodrigues Cordeiro <vrc_crv@outlook.com.br>
2026-04-27 22:48:48 -0300

    configurar rotas por componentes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { TarefaListaComponent } from './components/tarefa-lista/tarefa-lista.component';
+import { TarefaFormComponent } from './components/tarefa-form/tarefa-form.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'tarefas', pathMatch: 'full' },
+  { path: 'tarefas', component: TarefaListaComponent },
+  { path: 'tarefas/nova', component: TarefaFormComponent },
+  { path: 'tarefas/editar/:id', component: TarefaFormComponent },
+];
